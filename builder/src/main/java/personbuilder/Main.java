1b79c9f1e2cfcdaa6ffc15bc84191c0670d1cceb
VladimirTsvetov <96310995+VladimirTsvetov@users.noreply.github.com>
2023-03-19 19:27:01 +0300

    Update Main.java

--- builder/src/main/java/personbuilder/Main.java
+++ builder/src/main/java/personbuilder/Main.java
@@ -2,17 +2,7 @@ package personbuilder;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-           Создать builder для класса Person со следующими полями:
-           String firstName,
-           String lastName,
-           String middleName,
-           String country,
-           String address,
-           String phone,
-           int age,
-           String gender.
-       */
+        
         Person person = new Person
                 .Builder()
                 .withFirstName("Василий")
